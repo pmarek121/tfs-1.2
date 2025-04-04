c943f758ce7cd8bec2063f5f7544db48318c21ac
Tras <40568816+Tras@users.noreply.github.com>
2025-04-05 01:52:21 +0200

    Update Elvith.lua
    Makes him sell Elven Poetry Book if you already got one (in case you lose it).

--- data/npc/scripts/Elvith.lua
+++ data/npc/scripts/Elvith.lua
@@ -7,11 +7,55 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end 
+    local player = Player(cid)
+
+
+	if msgcontains(msg, 'songs of the forest') then
+		npcHandler:say({
+			'The last issue I had was bought by Randor Swiftfinger. He was banished through the hellgate and probably took the book with him ...',
+			'I would not recommend seeking him or the book there, but of course it is possible.'
+		}, cid)
+	elseif msgcontains(msg, 'love poem') then
+		npcHandler:say('Do you want to buy a poem scroll for 200 gold?', cid)
+		npcHandler.topic[cid] = 1
+	elseif msgcontains(msg, 'poetry book') and player:getStorageValue(Storage.ExplorerSociety.TheElvenPoetry) == 37 then
+		npcHandler:say('Do you want to buy the Elven Poetry Book for 500 gold?', cid)
+		npcHandler.topic[cid] = 2
+	elseif msgcontains(msg, 'song') then
+		npcHandler:say('Everything is a song. Life, death, history ... everything. To listen to the song of something is the first step to understand it.', cid)
+	elseif msgcontains(msg, 'yes') then
+		if npcHandler.topic[cid] == 1 then
+			npcHandler.topic[cid] = 0
+			if not player:removeMoney(200) then
+				npcHandler:say('You don\'t have enough money.', cid)
+				return true
+			end
+
+			player:addItem(5952, 1)
+			npcHandler:say('Here it is.', cid)
+		elseif npcHandler.topic[cid] == 2 then
+			npcHandler.topic[cid] = 0
+			if not player:removeMoney(500) then
+				npcHandler:say('You don\'t have enough money.', cid)
+				return true
+			end
+			
+			player:addItem(4855, 1)
+			npcHandler:say('Here it is.', cid)
+		end
+	end
+	return true
+end
+
 keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, text = 'I sell musical instruments of many kinds.'})
 keywordHandler:addKeyword({'instruments'}, StdModule.say, {npcHandler = npcHandler, text = 'I sell lyres, lutes, drums, and simple fanfares.'})
 keywordHandler:addKeyword({'music'}, StdModule.say, {npcHandler = npcHandler, text = 'Music is an attempt to condensate emotions in harmonies and save them for the times to come.'})
 keywordHandler:addKeyword({'time'}, StdModule.say, {npcHandler = npcHandler, text = 'Time has its own song. Close your eyes and listen to the symphony of the seasons.'})
-keywordHandler:addKeyword({'song'}, StdModule.say, {npcHandler = npcHandler, text = 'Everything is a song. Life, death, history ... everything. To listen to the song of something is the first step to understand it.'})
 keywordHandler:addKeyword({'melody'}, StdModule.say, {npcHandler = npcHandler, text = 'Everything is a song. Life, death, history ... everything. To listen to the song of something is the first step to understand it.'})
 keywordHandler:addKeyword({'elf'}, StdModule.say, {npcHandler = npcHandler, text = 'We are the most graceful of all races. We feel the music of the universe in our hearts and souls.'})
 keywordHandler:addKeyword({'kuridai'}, StdModule.say, {npcHandler = npcHandler, text = 'They could dig some halls for a big musical event, but they won\'t listen to me about that matter.'})
@@ -23,35 +67,6 @@ keywordHandler:addKeyword({'cenath'}, StdModule.say, {npcHandler = npcHandler, t
 keywordHandler:addKeyword({'troll'}, StdModule.say, {npcHandler = npcHandler, text = 'I went down to the mines and tried to lighten up their spirit, the foolish creatures did not listen to my songs, though.'})
 keywordHandler:addKeyword({'magic'}, StdModule.say, {npcHandler = npcHandler, text = 'Sorry, I don\'t feel like teaching magic today.'})
 keywordHandler:addKeyword({'hellgate'}, StdModule.say, {npcHandler = npcHandler, text = 'For the worst of crimes, criminals are cast into hellgate. It is said no one can return from there. Since it is not actually forbidden to enter hellgate, you might convince Elathriel to grant you entrance.'})
-
-local function creatureSayCallback(cid, type, msg)
-	if not npcHandler:isFocused(cid) then
-		return false
-	end
-
-	if msgcontains(msg, 'songs of the forest') then
-		npcHandler:say({
-			'The last issue I had was bought by Randor Swiftfinger. He was banished through the hellgate and probably took the book with him ...',
-			'I would not recommend seeking him or the book there, but of course it is possible.'
-		}, cid)
-	elseif msgcontains(msg, 'love poem') then
-		npcHandler:say('Do you want to buy a poem scroll for 200 gold?', cid)
-		npcHandler.topic[cid] = 1
-	elseif msgcontains(msg, 'yes') then
-		if npcHandler.topic[cid] == 1 then
-			npcHandler.topic[cid] = 0
-			local player = Player(cid)
-			if not player:removeMoney(200) then
-				npcHandler:say('You don\'t have enough money.', cid)
-				return true
-			end
-
-			player:addItem(5952, 1)
-			npcHandler:say('Here it is.', cid)
-		end
-	end
-	return true
-end
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:setMessage(MESSAGE_GREET, 'Ashari |PLAYERNAME|.')
